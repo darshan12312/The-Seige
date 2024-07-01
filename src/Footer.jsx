2ad0adc41270b90a4a2fa2a3e19353b0a05bbd1b
DarShan <darshandugad111@gmail.com>
2024-07-01 13:57:03 +0530

    SpellChange

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className=' w-full  h-[5vw] flex justify-center  ' >
       <div className=' border-t-2 border-red-500 w-[30vw] font-["Neue_Montreal"]  text-xl flex justify-center p-[1vw] text-white h-[3vw] '>
-        <p>© 2024 "THE SEIGE" All rights reserved </p>
+        <p>© 2024 "THE SIEGE" All rights reserved </p>
       </div>
     </div>
   )
